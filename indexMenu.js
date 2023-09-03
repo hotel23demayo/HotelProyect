852be1efde8d1fd55638a01b1fb9451f3b00c34d
hotel23demayo <hotel23demayo@suteba.org.ar>
2023-09-03 02:05:36 -0300

    Cambio logo 37 años

--- indexMenu.js
+++ indexMenu.js
@@ -1,13 +1,13 @@
 const almuerzoMenu = [
-            "Empanadas Criollas", "Ensalada Capresse", "Ensalada Lucho’s Cheff", "Tortillin de Papa", "Papas Cheddar", "Tortilla Española", "Papines Rellenos",
+            "Empanadas Criollas", "Ensalada Capresse", "Ensalada Lucho’s Cheff", "Tortillin de Papa", "Papas Cheddar", "Tortilla Española", "Papines Rellenos", "Ensalada Griega",
              "Bruschetta Mediterránea", "Ensalada Caesar", "Bastones de Muzzarella Rebozados", "Quesadillas con Pico de Gallo", "Quiché de Calabaza y Muzzarella", "Tacos de Pollo",
-              "Muzzarellitas", "Empanadas Capresse", "Fatay", "Mayonesa de Ave", "Croquetas de Espinaca y Queso Azul", "Jamón con Ensalada Rusa", "Tarta de Jamón y Queso", "Quesadillas",
-              "Mil Hojas de Papa Gratinada"
+              "Muzzarellitas", "Empanadas Capresse", "Fatay", "Mayonesa de Ave", "Croquetas de Espinaca y Queso Azul", "Jamón con Ensalada Rusa", "Tarta de Jamón y Queso", "Quesadillas", "Tartín de Verduras", "Mil Hojas de Papa y Panceta"
+              
         ];
         almuerzoMenu.push("Crunch de Pollo con Ali - Oli");
         
         const almuerzoSubMenu = [
-            " y Puerro", "con Ensaladilla", "con Panceta y Verdeo", " y Panceta", "con Ensalada Verde", "con Salsa Alexander", "con Ensaladilla Verde", " con Queso y Panceta"
+            " y Puerro", "con Ensaladilla", "con Panceta y Verdeo", " y Panceta", "con Ensalada Verde", "con Salsa Alexander", "con Ensaladilla Verde", " con Queso y Panceta", " con Pollo"
         ];
         almuerzoSubMenu.push(" ");
         
@@ -31,20 +31,21 @@ const almuerzoMenu = [
         almuerzoSubMenu.forEach(item => addToSelect(item, almuerzoSubMenuSelect));
         
         const cenaMenu = [
-            "Muslo Braseado al Limón", "Milanesa de Peceto", "Ravioli de Espinaca Mediterráneo", "Lasagna de Berenjenas",
-            "Suprema de Pollo", "Pechugas al Verdeo", "Tallarines Mediterráneo", "Tallarines a la Parisienne","Tallarines con Salsa Alfredo",
-            "Ñoquis a la Parisienne",
-            "Ravioli de Verdura", "Tallarines al Pesto","Raviolon de Espinaca y Ricota",
+            "Muslo Braseado al Limón", "Muslitos a la Portuguesa", "Milanesa de Ternera a la Napolitana", "Ravioli de Espinaca Mediterráneo", "Lasagna de Berenjenas", , "Bondiola Ahumada",
+            "Supremitas", "Pechugas al Verdeo", "Tallarines Mediterráneo", "Tallarines a la Parisienne","Tallarines con Salsa Alfredo",
+            "Ñoquis a la Parisienne", "Ñoquis de Papa",
+            "Ravioli de Verdura", "Tallarines al Pesto","Raviolon de Ricota y Verduras",
             "Muslo al Ajillo", "Lasagna de Carne y Verduras", "Suprema Mariland", "Carré de Cerdo a la Riojana" , "Pastel de Papa y Carne",
              "Vacío al Horno", "Pechugas al Puerro y Queso Azul", "Escalopines de Ternera", "Tallarines al Pesto de Albahaca", "Bondiola Grillada",
-              "Canelones de Verdura y Salsa Bechamel", "Pechugas Rellenas", "Caneloni a la Rossini", "Carne al Horno"
+              "Canelones de Verdura y Salsa Bechamel", "Pechugas Rellenas", "Caneloni a la Rossini", "Carne Braseada", "Pechugas Capresse",
+              "Wok de Verduras Mixto",
         ];
         cenaMenu.push("Tapa de Asado Braseada");
         
         const cenaSubMenu = [
-            "con Ensalada del Chef", "con Puré de Batatas", "con Puré de Papas", "con Arroz Pilaf", "a la Suiza", "con Queso Gratinado",
+            "con Ensalada del Chef", "con Puré de Batatas", "con Puré de Papas", "con Puré Marmolado", "con Arroz Pilaf", "a la Suiza", "con Queso Gratinado", "con Papas Especiadas", "con Vegetales al Horno",
              "con Panceta y Verdeo", "con Papas a la Provenzal", "con Ensaladilla", "con Ensalada", "con Papas al Natural", "con Salsa Pomodoro",
-             "(Champignon, Crema y Blanco de Ave)", "con Puré Mixto", "con Estofado", "con Filetto y Crema",
+             "(Champignon, Crema y Blanco de Ave)", "con Puré Mixto", "con Estofado", "con Filetto y Crema", "con Papas Rústicas", "con Papas a la Española","con Arroz Tae", "con Papas Fritas","con Ragú de Ternera", "con Salsa Bolognesa",
              "con Papas Bravas y Criolla", "con Tomates Asados", "con Salsa de Puerros", "con Risotto de Verduras", "con Papas a la Crema de Verdeo",
         ];
         
@@ -52,13 +53,13 @@ const almuerzoMenu = [
         
         const postreMenu = [
             "Flan de Naranja", "Helado de Americana","Helado de Chocolate", "Helado Almendrado", "Crepe de Manzanas y Canela",
-            "Budín de Pan", "Bombón Suizo", "Brownie de Chocolate", "Ensalada de Frutas", "Mousse de Chocolate", "Crepe de Frutas", "Flan con Crema"
-            , "Manzanas Asadas", "Duraznos en Almibar", "Compota de Manzanas", "Helado Tricolor", "Copa Helada Suteba", "Queso y Dulce Par", "Tiramisú"
+            "Budín de Pan", "Bombón Suizo", "Brownie de Chocolate", "Ensalada de Frutas", "Mousse de Chocolate", "Crepe de Frutas", "Flan Casero"
+            , "Manzanas Asadas", "Duraznos en Almibar", "Compota de Manzanas", "Helado Tricolor", "Copa Helada Suteba", "Queso y Dulce Par", "Tiramisú", "Peras al Borgoña"
         ];
         postreMenu.push("Crumble de Manzana");
         
         const postreSubMenu = [
-            "con Dulce de Leche", "con Praline de Frutos Secos", "con Nueces Tostadas", "con Salsa de Chocolate", "con Helado y Nueces Garrapiñadas",
+            "con Dulce de Leche", "con Praline de Frutos Secos", "con Nueces Tostadas", "con Salsa de Chocolate", "con Helado y Nueces Garrapiñadas", "y Nueces",
             "con Crema Chantilly", "con Salsa de Frutos Rojos", "de Chocolate", "Mixto", "con Helado", "y Crema de Limón", "con Crema y Canela",
              "y Crema de Caramelo","(Queso, Batata y Membrillo)", "y Crema de Arándanos"
         ];
